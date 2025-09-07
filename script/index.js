37b2ed7c79f3e808fda82ee227211f55805ddb16
Saniul <isaniul999@gmail.com>
2025-09-07 18:55:28 +0600

    added cart dynamic functionality

--- script/index.js
+++ script/index.js
@@ -18,7 +18,7 @@ const displayAllPlants=(plants)=>{
         cards.innerHTML=`
         <div class="h-[380px] bg-white shadow rounded-xl p-4">
               <div class="h-44  rounded-md mb-1"><img class="w-full h-full object-cover rounded-lg" src="${plant.image}" alt=""></div>
-              <h3 class="font-semibold text-lg mb-1">${plant.name}</h3>
+              <h3 onclick="boxModal(${plant.id})" class="font-semibold text-lg mb-1">${plant.name}</h3>
               <p class="text-[0.62rem] text-gray-600 mb-0">
                 ${plant.description}
               </p>
@@ -30,7 +30,7 @@ const displayAllPlants=(plants)=>{
               </div>
 
               <div class="mt-auto flex items-center justify-between">
-                <button class="w-full bg-green-600 text-white px-4 py-2 rounded-full hover:bg-green-700">
+                <button onclick="addToCart('${plant.name}',${plant.price})" class="w-full bg-green-600 text-white px-4 py-2 rounded-full hover:bg-green-700">
                   Add to Cart
                 </button>
               </div>
@@ -50,12 +50,13 @@ const category=()=>{
         category.categories.forEach(el=>{
             const innerBtn=document.createElement('div');
             innerBtn.innerHTML=`
-                <button id="cate_btn-${el.id}" class="block px-3 py-2 rounded-md hover:bg-green-600 hover:text-white w-full text-left">
+                <button id="cate-btn-${el.id}" class="block px-3 py-2 rounded-md hover:bg-green-600 hover:text-white w-full text-left">
                  ${el.category_name}
                 </button>
             `;
         innerBtn.setAttribute('onclick', `displayCategory(${el.id})`);
         btnList.appendChild(innerBtn);
+        innerBtn.classList.add('active');
         });
     });
 }
@@ -65,6 +66,92 @@ const displayCategory=(id)=>{
         displayAllPlants(el.plants);
     });
 }
+// "id": 1,
+// "image": "https://i.ibb.co.com/cSQdg7tf/mango-min.jpg",
+// "name": "Mango Tree",
+// "description": "A fast-growing tropical tree that produces delicious, juicy mangoes during summer. Its dense green canopy offers shade, while its sweet fruits are rich in vitamins and minerals.",
+// "category": "Fruit Tree",
+// "price": 500
+
+const boxModal=(id)=>{
+    const url=`https://openapi.programming-hero.com/api/plant/${id}`;
+    fetch(url).then(res=>res.json()).then(combine=>{
+      // console.log(combine.plants);
+        const modelBox=document.getElementById("modal-box");
+        const boxInModal=document.createElement('div');
+        modelBox.innerHTML = "";
+        boxInModal.innerHTML=`
+          <h3 class="text-2xl font-bold my-2">${combine.plants.name}</h3>
+          <div class="w-[450px] h-[300px] mb-1 mx-auto">
+            <img class="w-full h-full object-cover rounded-xl" src="${combine.plants.image}" alt="">
+          </div>
+          <p><span class="text-xl font-semibold my-2">Category :</span >${combine.plants.category}</p>
+          <p><span class="text-xl font-semibold my-2">Price :</span >${combine.plants.price}</p>
+          <p><span class="text-xl font-semibold my-2">Description :</span >${combine.plants.description}</p>
+          <div class="modal-action">
+            <form method="dialog">
+              <!-- if there is a button in form, it will close the modal -->
+              <button class="btn">Close</button>
+            </form>
+          </div>
+        `;
+        modelBox.appendChild(boxInModal);
+        my_modal.showModal();
+      });
+}
+let cartTotal = 0;
+
+const addToCart = (name, price) => {
+  cartTotal += price;
+  const cartParent = document.getElementById("cart-parent");
+
+  const cartChild = document.createElement("div");
+  cartChild.innerHTML = `
+    <div class="space-y-3 text-sm">
+      <div class="flex justify-between items-center p-2 bg-[#F0FDF4]">
+        <div class="space-y-2">
+          <h3 class="text-[0.875rem] font-semibold">${name}</h3>
+          <h3 class="text-gray-500">৳${price} x 1</h3>
+        </div>
+        <div>
+          <button class="p-3 remove-btn">x</button>
+        </div>
+      </div>
+    </div>
+  `;
+
+  cartParent.appendChild(cartChild);
+
+  cartChild.querySelector(".remove-btn").addEventListener("click", () => {
+    cartTotal -= price;
+    cartChild.remove();
+    countTotal();
+  });
+
+  countTotal();
+};
+
+const countTotal = () => {
+  let totalDivParent = document.getElementById("totalCount");
+  if (!totalDivParent) {
+    totalDivParent = document.createElement("div");
+    totalDivParent.id = "totalCount";
+    totalDivParent.className = "mt-3";
+    document.getElementById("cart-parent").appendChild(totalDivParent);
+  } else {
+    totalDivParent.remove();
+    document.getElementById("cart-parent").appendChild(totalDivParent);
+  }
+
+  totalDivParent.innerHTML = `
+    <hr class="my-3 text-gray-400" />
+    <div class="flex items-center justify-between font-semibold">
+      <span>Total:</span>
+      <span>৳${cartTotal}</span>
+    </div>
+  `;
+};
+
 
 category();
 allPlants();
